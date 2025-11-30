31372bb3ccd953aac0a8272efdedbe00245d2fa9
simplecyber <satyamok03@gmail.com>
2025-11-30 13:21:57 +0530

    # updaed next js

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "i.pinimg.com",
       },
+      {
+        protocol: "https",
+        hostname: "i0.wp.com", // <-- REQUIRED for your GIF
+      },
     ],
   },
 };
